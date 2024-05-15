60efec151af2d8be5a048d04e486f7c4b04e26b7
jostyace <jostyace@gmail.com>
2024-05-15 15:01:04 -0500

    fix

--- src/controllers/login-controller.js
+++ src/controllers/login-controller.js
@@ -13,11 +13,18 @@ export const verifyUser = (req, res) => {
     
     const { email, password } = req.body;
   
-    const user = users.find(u => u.email === email && u.password === password);
+    const user = users.find(u => u.email === email);
   
     if (user) {
-      const token = jwt.sign({ email: user.email }, key, { expiresIn: '1h' });
-      res.json({ token });
+
+        if (user.password ===  password) {
+            const token = jwt.sign({ email: user.email }, key, { expiresIn: '1h' });
+            res.json({ token });
+          }else {
+            res.status(403).json({ message: 'La contraseña es Incorrecta' });
+            return;
+        }
+
     } else {
       res.status(403).json({ message: 'No existe el Usuario' });
     }
